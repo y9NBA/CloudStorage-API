05b387c6833c307b417fa3361aa15302a7462819
y9NBA <asarkrylov76212@gmail.com>
2024-12-28 09:03:29 +0300

    Init project + create folders

---
+++ src/test/java/org/y9nba/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.y9nba.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
