3e673b51bf221405775bdd3bdc99e76e35e121fc
y9NBA <asarkrylov76212@gmail.com>
2025-01-23 12:33:24 +0300

    Update Role.java for normal enum

--- src/main/java/org/y9nba/app/model/user/Role.java
+++ src/main/java/org/y9nba/app/model/user/Role.java
@@ -1,18 +1,7 @@
 package org.y9nba.app.model.user;
 
 public enum Role {
-
-    ROLE_USER("ROLE_USER"),
-    ROLE_ADMIN("ROLE_ADMIN");
-
-    public final String roleName;
-
-    Role(String roleName) {
-        this.roleName = roleName;
-    }
-
-    public final String getRoleName() {
-        return this.roleName;
-    }
+    ROLE_USER(),
+    ROLE_ADMIN();
 }
 
