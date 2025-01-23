4174f3bab08c3786f89acb6b2691377016813e9e
y9NBA <asarkrylov76212@gmail.com>
2025-01-23 12:21:21 +0300

    Add field roleName as final String and getter of him

--- src/main/java/org/y9nba/app/model/user/Role.java
+++ src/main/java/org/y9nba/app/model/user/Role.java
@@ -5,7 +5,14 @@ public enum Role {
     ROLE_USER("ROLE_USER"),
     ROLE_ADMIN("ROLE_ADMIN");
 
-    Role(String name) {
+    public final String roleName;
+
+    Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public final String getRoleName() {
+        return this.roleName;
     }
 }
 
