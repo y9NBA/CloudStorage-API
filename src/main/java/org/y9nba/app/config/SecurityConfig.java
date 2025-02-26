9787b396a3f382b713b1e5208dbe51660afbf334
y9NBA <asarkrylov76212@gmail.com>
2025-02-26 23:44:39 +0300

    update: обновил конфиги под auth составляющую

--- src/main/java/org/y9nba/app/config/SecurityConfig.java
+++ src/main/java/org/y9nba/app/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package org.y9nba.app.config;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -12,12 +11,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.y9nba.app.security.CustomAccessDeniedHandler;
+import org.y9nba.app.security.CustomLogoutHandler;
+import org.y9nba.app.security.JwtFilter;
 
 import java.io.IOException;
 import java.util.List;
@@ -27,7 +31,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final ApplicationContext applicationContext;
+    private final JwtFilter jwtFilter;
+    private final CustomLogoutHandler customLogoutHandler;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -47,24 +53,28 @@ public class SecurityConfig {
                         .configurationSource(corsConfigurationSource()))
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionManagementCustomizer -> sessionManagementCustomizer
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//                .exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
-//                        httpSecurityExceptionHandlingConfigurer
-//                                .authenticationEntryPoint(
-//                                        (request, response, authException) -> unauthorizedResponse(response)
-//                                )
-//                                .accessDeniedHandler(
-//                                        (request, response, authException) -> unauthorizedResponse(response)
-//                                )
-//                )
-//                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-//                        authorizationManagerRequestMatcherRegistry
-//                                .requestMatchers("**")
-//                                .authenticated()
-//                                .anyRequest()
-//                                .permitAll()
-//                );
-//                .addFilterBefore(new JwtTokenFilter(JwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
+                        httpSecurityExceptionHandlingConfigurer
+                                .authenticationEntryPoint(
+                                        (request, response, authException) -> unauthorizedResponse(response)
+                                )
+                                .accessDeniedHandler(customAccessDeniedHandler)
+                )
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("auth/**", "general/**", "css/**",
+                                    "swagger-ui/**", "v3/api-docs/**")
+                            .permitAll();
+                    auth.anyRequest().authenticated();
+                        }
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(log -> {
+                    log.logoutUrl("/logout");
+                    log.addLogoutHandler(customLogoutHandler);
+                    log.logoutSuccessHandler((request, response, authentication) ->
+                            SecurityContextHolder.clearContext());
+                });
 
         return httpSecurity.build();
     }
