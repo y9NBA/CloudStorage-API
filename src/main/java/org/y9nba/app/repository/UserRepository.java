582a1239e2638caf970006e21916baabc125a833
y9NBA <asarkrylov76212@gmail.com>
2025-02-26 23:16:36 +0300

    create: создаÐupdate: немноÐдобавил два метода ана exists для User по email и username

--- src/main/java/org/y9nba/app/repository/UserRepository.java
+++ src/main/java/org/y9nba/app/repository/UserRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.y9nba.app.model.UserModel;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 
-    UserModel findByUsername(String username);
-    UserModel findByEmail(String email);
+    Optional<UserModel> findByUsername(String username);
+    Optional<UserModel> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
