5e26096e4ab1a8d74aeda3055c283d60dbdd7c07
y9NBA <asarkrylov76212@gmail.com>
2025-04-23 18:32:24 +0300

    add some methods

--- src/main/java/org/y9nba/app/repository/FileRepository.java
+++ src/main/java/org/y9nba/app/repository/FileRepository.java
@@ -4,10 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.y9nba.app.model.FileModel;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Repository
 public interface FileRepository extends JpaRepository<FileModel, Long> {
 
-    Set<FileModel> getFileModelsByUserId(Long userId);
+    Set<FileModel> getFileModelsByUser_Username(String username);
+    Set<FileModel> getFileModelsByUser_UsernameAndUrlContaining(String username, String url);
+    Optional<FileModel> getFileModelByUser_UsernameAndUrl(String username, String url);
+    boolean existsByUrl(String url);
 }
