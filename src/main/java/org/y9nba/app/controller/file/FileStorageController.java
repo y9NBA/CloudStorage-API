fd49991d6601ebc5b84e294fbb67324e1f030715
y9NBA <asarkrylov76212@gmail.com>
2025-04-24 14:54:14 +0300

    update: add AuthPrincipal and replace username on userId

--- src/main/java/org/y9nba/app/controller/file/FileStorageController.java
+++ src/main/java/org/y9nba/app/controller/file/FileStorageController.java
@@ -1,19 +1,16 @@
 package org.y9nba.app.controller.file;
 
-import io.minio.messages.Bucket;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.y9nba.app.dto.file.FileDto;
 import org.y9nba.app.model.FileModel;
-import org.y9nba.app.security.JwtService;
+import org.y9nba.app.model.UserModel;
 import org.y9nba.app.service.impl.FileStorageServiceImpl;
 import org.y9nba.app.service.impl.StorageServiceImpl;
 
@@ -28,12 +25,10 @@ public class FileStorageController {
 
     private final StorageServiceImpl storageService;
     private final FileStorageServiceImpl fileStorageService;
-    private final JwtService jwtService;
 
-    public FileStorageController(StorageServiceImpl storageService, FileStorageServiceImpl fileStorageService, JwtService jwtService) {
+    public FileStorageController(StorageServiceImpl storageService, FileStorageServiceImpl fileStorageService) {
         this.storageService = storageService;
         this.fileStorageService = fileStorageService;
-        this.jwtService = jwtService;
     }
 
     @GetMapping(path = "/buckets")
@@ -42,15 +37,14 @@ public class FileStorageController {
     }
 
     @PostMapping(path = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public FileDto uploadFile(@RequestPart(value = "file", required = false) MultipartFile file, @AuthenticationPrincipal UserDetails userDetails) {
-        FileModel fileModel = fileStorageService.uploadFile(userDetails.getUsername(), file);   // TODO: сделать проверку exist и на основе этого обновлять файл или создавать
-
+    public FileDto uploadFile(@RequestPart(value = "file", required = false) MultipartFile file, @RequestParam(required = false) String folderUrl, @AuthenticationPrincipal UserModel userModel) {
+        FileModel fileModel = fileStorageService.uploadFile(userModel.getId(), file, folderUrl);
         return new FileDto(fileModel);
     }
 
     @GetMapping(path = "/download/{fileName}")
-    public ResponseEntity<InputStreamResource> downloadFile(@PathVariable String fileName, @AuthenticationPrincipal UserDetails userDetails, HttpServletResponse response) {
-        InputStream inputStream = fileStorageService.downloadFile(userDetails.getUsername(), fileName);   // TODO: сделать проверку доступа к файлу или вынести данную логику в отдельный контроллер
+    public ResponseEntity<InputStreamResource> downloadFile(@PathVariable String fileName, @AuthenticationPrincipal UserModel userModel, HttpServletResponse response) {
+        InputStream inputStream = fileStorageService.downloadFile(userModel.getId(), fileName);   // TODO: сделать проверку доступа к файлу или вынести данную логику в отдельный контроллер
         InputStreamResource resource = new InputStreamResource(inputStream);
         String encodedFileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8).replaceAll("\\+", "%20");
 
