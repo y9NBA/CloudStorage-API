f2f1ece0cc73a8e083582c60815ad5f814c79456
y9NBA <asarkrylov76212@gmail.com>
2025-05-06 15:32:50 +0300

    mega-update: add and update methods for work with access, add access control system for files and operations for them

--- src/main/java/org/y9nba/app/service/FileStorageService.java
+++ src/main/java/org/y9nba/app/service/FileStorageService.java
@@ -3,10 +3,10 @@ package org.y9nba.app.service;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
+import org.y9nba.app.constant.Access;
 import org.y9nba.app.dto.file.FileCreateDto;
+import org.y9nba.app.dto.file.FileInputStreamWithAccessDto;
 import org.y9nba.app.dto.file.FileUpdateDto;
-import org.y9nba.app.dto.share.ExpireRequestDto;
-import org.y9nba.app.dto.share.SharedUrlResponseDto;
 import org.y9nba.app.model.FileModel;
 
 import java.io.InputStream;
@@ -14,24 +14,27 @@ import java.util.Set;
 
 public interface FileStorageService {
     FileModel uploadFile(Long userId, MultipartFile file, String folderURL);
-    InputStream downloadFile(Long userId, String fileName, String folderURL);
-    InputStream downloadFileByAccess(Long userId, Long fileId);
+    FileModel uploadFileByAccess(Long userId, MultipartFile file, String bucketName, String fileName, String folderURL);
+    FileInputStreamWithAccessDto downloadFile(Long userId, String fileName, String folderURL);
+    FileInputStreamWithAccessDto downloadFileByAccess(Long userId, String bucketName, String fileName, String folderURL);
     FileModel moveFileOnNewUrl(Long userId, String fileName, String newFolderURL, String oldFolderURL);
     FileModel copyExistingFile(Long userId, String fileName, String folderURL);
     FileModel renameFile(Long userId, String fileName, String newFileName, String folderURL);
     FileModel save(FileCreateDto dto);
     FileModel update(FileUpdateDto dto);
     String deleteFile(Long userId, String fileName, String folderURL);
-    FileModel findById(Long id);
-    FileModel findByUserIdAndUrl(Long userId, String url);
-    FileModel findFile(Long userId, String fileName, String folderURL);
+    FileModel giveAccessOnFileForUser(Long userId, String fileName, String folderURL, Long collaboratorUserId, Access access);
+    FileModel revokeAccessOnFileForUser(Long userId, String fileName, String folderURL, Long collaboratorUserId);
+    FileModel revokeAllAccessOnFile(Long userId, String fileName, String folderURL);
+    FileModel makeFilePublic(Long userId, String fileName, String folderURL);
+    FileModel makeFilePrivate(Long userId, String fileName, String folderURL);
     Set<FileModel> findByUserId(Long userId);
     Set<FileModel> findByUserIdAndFolderUrl(Long userId, String folderURL);
-    FileModel findOwnerByFileId(Long userId, Long fileId);
+    FileModel findOwnerByFileId(Long userId, String bucketName, String fileName, String folderURL);
     Set<FileModel> findOwnerByUserId(Long userId);
     Set<FileModel> findOwnerByUserIdAndFolderUrl(Long userId, String folderURL);
     boolean existsByURL(String url);
-    SharedUrlResponseDto getSharedUrlForFile(ExpireRequestDto expireRequestDto, Long userId, String fileName, String folderURL);
-    ResponseEntity<InputStreamResource> getResourceByInputStream(InputStream inputStream, String fileName);
+    ResponseEntity<InputStreamResource> getResourceForViewByInputStream(FileInputStreamWithAccessDto dto, FileModel fileModel);
+    ResponseEntity<InputStreamResource> getResourceForDownloadByInputStream(FileInputStreamWithAccessDto dto, FileModel fileModel);
     void refreshFiles(Long userId);
 }
