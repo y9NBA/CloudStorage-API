569a10a8b2e4e7cf8f3662433bc6b707ca168f60
y9NBA <asarkrylov76212@gmail.com>
2025-05-20 21:56:15 +0300

    create: асинхронность для рассылки писем, так как они очень долгие и стопают сильно основной поток

---
+++ src/main/java/org/y9nba/app/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package org.y9nba.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync(proxyTargetClass = true)
+public class AsyncConfig {
+
+    @Bean(name = "emailTaskExecutor")
+    public Executor emailTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("EmailThread-");
+        executor.initialize();
+        return executor;
+    }
+}
