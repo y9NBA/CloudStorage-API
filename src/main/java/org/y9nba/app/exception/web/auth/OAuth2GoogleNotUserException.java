e70612ebfa4e834024a19a6684e00ce380d807f6
y9NBA <asarkrylov76212@gmail.com>
2025-05-20 22:06:39 +0300

    create: exceptions под новый функционал

---
+++ src/main/java/org/y9nba/app/exception/web/auth/OAuth2GoogleNotUserException.java
@@ -0,0 +1,11 @@
+package org.y9nba.app.exception.web.auth;
+
+import org.springframework.http.HttpStatus;
+import org.y9nba.app.exception.web.AbstractException;
+
+public class OAuth2GoogleNotUserException extends AbstractException {
+
+    public OAuth2GoogleNotUserException() {
+        super("Произошла какая-то ошибка при авторизации через Google", HttpStatus.FORBIDDEN);
+    }
+}
