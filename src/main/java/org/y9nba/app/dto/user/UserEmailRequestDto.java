3af8b3a9cec195624d18df3781ece86a0489d908
y9NBA <asarkrylov76212@gmail.com>
2025-05-20 21:59:13 +0300

    create: dto для последних контроллеров

---
+++ src/main/java/org/y9nba/app/dto/user/UserEmailRequestDto.java
@@ -0,0 +1,10 @@
+package org.y9nba.app.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserEmailRequestDto {
+    private final String email;
+}
