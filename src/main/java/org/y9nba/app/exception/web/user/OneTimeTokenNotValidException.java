71b9dfe9cae19ec507262f8c3f4875272a2adf4f
y9NBA <asarkrylov76212@gmail.com>
2025-05-20 21:47:20 +0300

    update and create: - Доделал основные классы и методы для взаимодействия с одноразовыми токенами и их работы - Обновил jwtService, добавил методы для работы с одноразовыми токенами

---
+++ src/main/java/org/y9nba/app/exception/web/user/OneTimeTokenNotValidException.java
@@ -0,0 +1,11 @@
+package org.y9nba.app.exception.web.user;
+
+import org.springframework.http.HttpStatus;
+import org.y9nba.app.exception.web.AbstractException;
+
+public class OneTimeTokenNotValidException extends AbstractException {
+
+    public OneTimeTokenNotValidException() {
+        super("Данная ссылка устарела или уже была использована.", HttpStatus.GONE);
+    }
+}
