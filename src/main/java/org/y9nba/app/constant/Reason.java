023aa63cf8229a28813c70c355a23c6cca1ead78
y9NBA <asarkrylov76212@gmail.com>
2025-06-03 00:21:45 +0300

    update and create: Role have authorities, new enum Reason for warnings

---
+++ src/main/java/org/y9nba/app/constant/Reason.java
@@ -0,0 +1,5 @@
+package org.y9nba.app.constant;
+
+public enum Reason {
+    UNSATISFACTORY_PUBLIC_FILE
+}
--- src/main/java/org/y9nba/app/constant/Role.java
+++ src/main/java/org/y9nba/app/constant/Role.java
@@ -1,8 +1,42 @@
 package org.y9nba.app.constant;
 
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
 public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_SUPER_ADMIN
+    ROLE_USER(Set.of(
+            "MANIPULATE_STORAGE",
+            "UPDATE_USERNAME",
+            "UPDATE_PASSWORD",
+            "UPDATE_EMAIL",
+            "UPDATE_PROFILE",
+            "DELETE_PROFILE"
+    )),
+    ROLE_ADMIN(Set.of(
+            "VIEW_PUBLIC_FILES",
+            "REVOKE_PUBLIC_FILES",
+            "BAN_HAMMER",
+            "INFO_USERS",
+            "UPDATE_USERNAME",
+            "UPDATE_PASSWORD",
+            "DELETE_PROFILE"
+    )),
+    ROLE_SUPER_ADMIN(Set.of(
+            "VIEW_PUBLIC_FILES",
+            "REVOKE_PUBLIC_FILES",
+            "BAN_HAMMER",
+            "INFO_USERS",
+            "INFO_ADMINS",
+            "CREATE_ADMIN",
+            "DELETE_ADMIN"
+    ));
+
+    private final Set<String> authorities;
+
+    Role(Set<String> authorities) {
+        this.authorities = authorities;
+    }
 }
 
