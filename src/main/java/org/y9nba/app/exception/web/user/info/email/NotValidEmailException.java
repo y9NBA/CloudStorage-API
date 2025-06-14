f6d6090feafdcc27c27e96974ddc355329dee7d7
y9NBA <asarkrylov76212@gmail.com>
2025-06-14 20:02:45 +0300

    update: переместил исключения пользака, связанные с его полям, по пакетам

--- src/main/java/org/y9nba/app/exception/web/user/info/email/NotValidEmailException.java
+++ src/main/java/org/y9nba/app/exception/web/user/info/email/NotValidEmailException.java
@@ -1,4 +1,4 @@
-package org.y9nba.app.exception.web.auth;
+package org.y9nba.app.exception.web.user.info.email;
 
 import org.springframework.http.HttpStatus;
 import org.y9nba.app.exception.web.AbstractException;
@@ -6,6 +6,6 @@ import org.y9nba.app.exception.web.AbstractException;
 public class NotValidEmailException extends AbstractException {
 
     public NotValidEmailException() {
-        super("Не очень похоже на корректный email", HttpStatus.BAD_REQUEST);
+        super("Некорректный адрес электронной почты", HttpStatus.BAD_REQUEST);
     }
 }
